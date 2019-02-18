cc5ac7d9abd93341352bcd8b00111f5247f1494e
benleahy91 <benleahy91@gmail.com>
2019-02-18 16:39:06 -0500

    dynamic nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-// import axios from '../../axios';
+// import axios from 'axios';
 import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
 
-	render () {
+	render() {
 		return (
 			<div className="Blog">
 				<header>
@@ -17,27 +16,31 @@ class Blog extends Component {
 						<ul>
 							<li>
 								<NavLink
-									to="/"
+									to="/posts/"
 									exact
+									activeClassName="my-active"
 									activeStyle={{
+										color: '#fa923f',
 										textDecoration: 'underline'
-									}}>Home
+									}}>Posts
 								</NavLink>
 							</li>
-							<li><NavLink to={{
-								pathname: '/new-post',
-								hash: '#submit',
-								search: '?quick-submit=true'
-							}}>New Post</NavLink></li>
+							<li>
+								<NavLink to={{
+									pathname: '/new-post',
+									hash: '#submit',
+									search: '?quick-submit=true'
+									}}>New Post
+								</NavLink>
+							</li>
 						</ul>
 					</nav>
 				</header>
 				{/* <Route path="/" exact render={() => <h1>Home</h1>} />
 				<Route path="/" exact render={() => <h1>Home</h1>} /> */}
 				<Switch>
-					<Route path="/" exact component={ Posts } />
 					<Route path="/new-post" component={ NewPost } />
-					<Route path="/:id" exact component={ FullPost } />
+					<Route path="/posts" component={ Posts } />
 				</Switch>
 			</div>
 		);
--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -9,19 +9,28 @@ class FullPost extends Component {
 	};
 
 	componentDidMount () {
-		console.log(this.props.match.params.id);
-		if ( this.props.match.params.id ) {
-			if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id) ) {
-				axios.get( '/' + this.props.match.params.id )
-					.then( response => {
+		console.log(this.props);
+		this.loadData();
+	};
+
+	componentDidUpdate() {
+		this.loadData();
+	};
+
+	loadData () {
+		if (this.props.match.params.id) {
+			if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id) ) {
+				axios.get('/posts/' + this.props.match.params.id)
+					.then(response => {
+						// console.log(response);
 						this.setState({ loadedPost: response.data });
 					});
 			};
 		};
-	};
+	}
 
 	deletePostHandler = () => {
-		axios.delete('/posts/' + this.props.id)
+		axios.delete('/posts/' + this.props.match.params.id)
 			.then(response => {
 				console.log(response);
 			});
@@ -29,16 +38,16 @@ class FullPost extends Component {
 
 	render () {
 		let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-		if ( this.props.id ) {
+		if (this.props.match.params.id) {
 			post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
 		};
-		if ( this.state.loadedPost ) {
+		if (this.state.loadedPost) {
 			post = (
 				<div className="FullPost">
 					<h1>{ this.state.loadedPost.title }</h1>
 					<p>{ this.state.loadedPost.body }</p>
 					<div className="Edit">
-						<button onClick={this.deletePostHandler } className="Delete">Delete</button>
+						<button onClick={ this.deletePostHandler } className="Delete">Delete</button>
 					</div>
 				</div>
 			);
